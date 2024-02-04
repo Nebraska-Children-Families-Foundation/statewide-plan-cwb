065a29e9f959f538b8db16ce4c9621c4b5138dbb
Zachary Nelson <hello@zacharynelson.me>
2024-02-04 10:21:22 -0600

    update user model & settings

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import AppUser
 from django.contrib.auth.admin import UserAdmin
-from django.forms import TextInput, Textarea
 
 
 class AppUserAdmin(UserAdmin):
@@ -17,7 +16,8 @@ class AppUserAdmin(UserAdmin):
     ]
 
     add_fieldsets = [
-        (None, {'fields': ['email', 'username', 'first_name', 'last_name', 'password1', 'password2', 'is_active', 'is_staff']})
+        (None, {'fields': ['email', 'username', 'first_name', 'last_name', 'password1', 'password2', 'is_active',
+                           'is_staff']})
     ]
 
 
