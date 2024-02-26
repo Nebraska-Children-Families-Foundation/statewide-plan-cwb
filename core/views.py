e2f5873d73abfc353051ebd27461255e8a3e94c1
Zachary Nelson <hello@zacharynelson.me>
2024-02-25 18:45:25 -0600

    clean up organization of html files in core

--- core/views.py
+++ core/views.py
@@ -4,4 +4,4 @@ from .models import Goal, Objective, Strategy
 def home(request):
     goals = Goal.objects.all().prefetch_related('objective_set__strategy_set')
     context = {'goals': goals}
-    return render(request, 'home.html', context)
+    return render(request, 'core/home.html', context)
