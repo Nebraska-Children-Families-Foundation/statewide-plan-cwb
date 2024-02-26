2cefc35380d1c76862bc74521e46a9c36daa4faf
Zachary Nelson <znelson@nebraskachildren.org>
2024-02-26 13:46:01 -0600

    add pages and urls for front-end functionality

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('communication-plan/', views.communication_plan, name='communication_plan'),
+    path('goals/', views.goals, name='goals'),
+    path('strategies-objectives/', views.strategies_objectives, name='strategies_objectives'),
+    path('community-activities/', views.community_activities, name='community_activities'),
+    path('partner-activities/', views.partner_activities, name='partner_activities'),
+    path('create-community-activity/', views.community_activities, name='create_community_activity'),
+    path('create-partner-activity/', views.partner_activities, name='create_partner_activity'),
+    path('community-collaboratives/', views.community_collaboratives, name='community_collaboratives'),
+    path('activities', views.activities, name='activities')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
 from .models import Goal, Objective, Strategy
 
+
 def home(request):
     goals = Goal.objects.all().prefetch_related('objective_set__strategy_set')
     context = {'goals': goals}
     return render(request, 'core/home.html', context)
+
+
+def about(request):
+    return render(request, 'core/about.html')
+
+
+def communication_plan(request):
+    return render(request, 'core/communication-plan.html')
+
+
+def community_activities(request):
+    return render(request, 'core/community-activities.html')
+
+
+def partner_activities(request):
+    return render(request, 'core/partner-activities.html')
+
+
+def community_collaboratives(request):
+    return render(request, 'core/community-collaboratives.html')
+
+
+def strategies_objectives(request):
+    return render(request, 'core/strategies-objectives.html')
+
+
+def goals(request):
+    return render(request, 'core/goals.html')
+
+
+def activities(request):
+    return render(request, 'core/activities.html')
