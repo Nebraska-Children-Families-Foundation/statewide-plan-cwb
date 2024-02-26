fa74b31b467cf1b45c62d4ceab018df3cb082898
Zachary Nelson <hello@zacharynelson.me>
2024-02-25 19:50:02 -0600

    add django-widget-tweaks for form styling

--- statewideplanCWB/settings.py
+++ statewideplanCWB/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'smart_selects',
+    'widget_tweaks',
     'core',
     'users',
 ]
@@ -121,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
