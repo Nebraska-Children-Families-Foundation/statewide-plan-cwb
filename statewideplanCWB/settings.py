35403284e34eacb31272ae941cb6c3bc79ec08cb
Zach Nelson <znelson@nebraskachildren.org>
2024-03-04 00:18:41 -0600

    create static files for whitenoise

--- statewideplanCWB/settings.py
+++ statewideplanCWB/settings.py
@@ -145,7 +145,9 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    str(BASE_DIR / 'static'),
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
