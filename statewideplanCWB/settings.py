aee9ad7f08fabe8d79b0f763726750ba279ec82d
Zachary Nelson <znelson@nebraskachildren.org>
2024-03-04 22:18:34 -0600

    update login/out redirects

--- statewideplanCWB/settings.py
+++ statewideplanCWB/settings.py
@@ -165,5 +165,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.AppUser'
 
 # Login Redirection
-LOGIN_REDIRECT_URL = '/home/'
-LOGOUT_REDIRECT_URL = '/home/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
