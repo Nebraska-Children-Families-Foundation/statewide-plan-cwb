491d8735682d11b43e77b6368f5f19e7411cbcbc
Zachary Nelson <znelson@nebraskachildren.org>
2024-03-14 16:28:11 -0500

    add comments for clarity

--- core/models.py
+++ core/models.py
@@ -1,5 +1,14 @@
-from .plan_work import *
-from .plan_actors import *
-from .standardization import *
-from .measurement import *
-from . alignment import *
+"""
+core/models.py
+This file acts as an entry point for all models in the `core` application.
+Individual model categories are organized into separate modules within this
+application. Refer to the respective modules for specific model implementations.
+"""
+
+# Importing models from submodules
+from .standardization.models import *  # Handles models for standardizing dropdowns and selections
+from .measurement.models import *      # Models related to performance measurements
+from .alignment.models import *        # Models for alignment with external stakeholders
+from .plan_actors.models import *      # Models for actors involved in the plan
+from .plan_work.models import *        # Models for the core working aspects of the plan
+from .relationships.models import *    # Models for handling relationships between different entities
