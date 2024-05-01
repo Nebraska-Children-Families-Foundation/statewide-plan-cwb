ae687c30060169d87e8d6f940711fe57d7e1cd14
Zachary Nelson <znelson@nebraskachildren.org>
2024-05-01 13:21:40 -0500

    update related goal & objective dropdowns with names

--- core/forms.py
+++ core/forms.py
@@ -76,49 +76,16 @@ class PartnerActivityForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(PartnerActivityForm, self).__init__(*args, **kwargs)
-        self.fields['completedby_year'].empty_label = "Select Year"
-        self.fields['completedby_quarter'].empty_label = "Select Quarter"
 
-        self.fields['commitment_status'].widget.attrs.update({'class': 'form-control col-md-4'})
-        self.fields['completedby_year'].widget.attrs.update({'class': 'form-control col-md-4'})
-        self.fields['completedby_quarter'].widget.attrs.update({'class': 'form-control col-md-4'})
+        # Customize querysets for related fields
+        self.fields['related_goal'].queryset = Goal.objects.all().order_by('goal_number')
+        self.fields['related_objective'].queryset = Objective.objects.all().order_by('objective_number')
+        self.fields['related_strategy'].queryset = Strategy.objects.all().order_by('strategy_number')
 
-        # Customize querysets for related fields with annotations if necessary
-        self.fields['related_goal'].queryset = Goal.objects.annotate(
-            custom_label=F('goal_number')
-        ).order_by('goal_number')
-
-        self.fields['related_objective'].queryset = Objective.objects.annotate(
-            custom_label=F('objective_number')
-        ).order_by('objective_number')
-
-        self.fields['related_strategy'].queryset = Strategy.objects.annotate(
-            custom_label=F('strategy_number')
-        ).order_by('strategy_number')
-
-        self.fields['related_systempartner'].queryset = SystemPartner.objects.annotate(
-            custom_label=F('system_partner_name')
-        ).order_by('system_partner_name')
-
-        # Use custom labels for dropdown display
-        self.fields['related_goal'].label_from_instance = lambda \
-            obj: f"Goal {getattr(obj, 'custom_label', obj.goal_number)}"
-        self.fields['related_objective'].label_from_instance = lambda \
-            obj: f"Objective {getattr(obj, 'custom_label', obj.objective_number)}"
-        self.fields['related_strategy'].label_from_instance = lambda \
-            obj: f"Strategy {getattr(obj, 'custom_label', obj.strategy_number)}"
-        self.fields['related_systempartner'].label_from_instance = lambda obj: getattr(obj, 'custom_label',
-                                                                                       obj.system_partner_name)
-
-        # Placeholders for dropdowns
-        self.fields['related_systempartner'].choices = [('', 'Please select the related System Partner')] + list(
-            self.fields['related_systempartner'].choices)
-        self.fields['related_goal'].choices = [('', 'Please select the related Goal')] + list(
-            self.fields['related_goal'].choices)
-        self.fields['related_objective'].choices = [('', 'Please select the related Objective')] + list(
-            self.fields['related_objective'].choices)
-        self.fields['related_strategy'].choices = [('', 'Please select the related Strategy')] + list(
-            self.fields['related_strategy'].choices)
+        # Concatenate goal_number with goal_name, and similar for objective and strategy
+        self.fields['related_goal'].label_from_instance = lambda obj: f"Goal {obj.goal_number}: {obj.goal_name}"
+        self.fields['related_objective'].label_from_instance = lambda obj: f"Obj. {obj.objective_number}: {obj.objective_name}"
+        self.fields['related_strategy'].label_from_instance = lambda obj: f"{obj.strategy_number}: {obj.strategy_name}"
 
 
 class NcffActivityForm(forms.ModelForm):
