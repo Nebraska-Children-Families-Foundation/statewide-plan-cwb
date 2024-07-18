ac4824b4f2ceb921e6a20d6026d73421eecaa867
Zachary Nelson <znelson@nebraskachildren.org>
2024-07-17 21:12:25 -0500

    add reset password box

--- users/admin.py
+++ users/admin.py
@@ -8,14 +8,15 @@ class AppUserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',
+                                    'must_reset_password')}),
         ('Important dates', {'fields': ('last_login', 'start_date')}),
         ('Associations', {'fields': ('community_collaborative', 'system_partner', 'member_type')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
+            'fields': ('email', 'password1', 'password2', 'must_reset_password'),
         }),
         ('Associations', {'fields': ('community_collaborative', 'system_partner', 'member_type')}),
     )
