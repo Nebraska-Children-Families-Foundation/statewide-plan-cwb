3dbe8ffd646a879bc89ed8cc3c15ac473655ebd5
Zachary Nelson <znelson@nebraskachildren.org>
2024-07-17 22:41:14 -0500

    update user form layout

--- users/admin.py
+++ users/admin.py
@@ -6,20 +6,20 @@ from .models import AppUser
 class AppUserAdmin(BaseUserAdmin):
     model = AppUser
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password', 'must_reset_password')}),
         ('Personal info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',
-                                    'must_reset_password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
         ('Important dates', {'fields': ('last_login', 'start_date')}),
         ('Associations', {'fields': ('community_collaborative', 'system_partner', 'member_type')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'must_reset_password', 'is_active', 'is_staff',
-                       'is_superuser'),
+            'fields': ('email', 'password1', 'password2', 'must_reset_password'),
         }),
-        ('Associations', {'fields': ('community_collaborative', 'system_partner', 'member_type')}),
+        ('Roles', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Associations', {'fields': ('member_type', 'community_collaborative', 'system_partner')}),
     )
     list_display = ('email', 'first_name', 'last_name', 'is_staff', 'member_type')
     search_fields = ('email', 'first_name', 'last_name')
