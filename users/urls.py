70a75825e99bf1cce3fc0d05440f1fea90155890
Zachary Nelson <znelson@nebraskachildren.org>
2024-07-23 20:39:44 -0500

    drop PasswordResetView in favor of built-in

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CustomLoginView, PasswordResetView
+from .views import CustomLoginView
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
 
