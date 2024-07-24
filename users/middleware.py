9e71358161a10eac442052dfb4fee616ab3d936a
Zachary Nelson <znelson@nebraskachildren.org>
2024-07-23 20:17:16 -0500

    create middleware for password reset check

---
+++ users/middleware.py
@@ -0,0 +1,13 @@
+from django.shortcuts import redirect
+
+
+class CheckPasswordResetMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user.is_authenticated and request.user.must_reset_password:
+            if request.path not in [reverse('password_change'), reverse('password_change_done')]:
+                return redirect('password_change')
+        response = self.get_response(request)
+        return response
