3a8dde9ca39c3597aae9cf21a685bd5df1f370ee
Zachary Nelson <znelson@nebraskachildren.org>
2024-07-23 20:18:05 -0500

    add middleware for password reset to settings

--- statewideplanCWB/settings.py
+++ statewideplanCWB/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'users.middleware.CheckPasswordResetMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
