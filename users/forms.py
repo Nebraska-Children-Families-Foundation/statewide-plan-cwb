50eaceaccc1cd6588cfa03ccd7cdbe668d429d47
Zachary Nelson <znelson@nebraskachildren.org>
2024-07-23 20:31:25 -0500

    drop SetPasswordForm

--- users/forms.py
+++ users/forms.py
@@ -1,28 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, SetPasswordForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import gettext_lazy as _
 
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.EmailField(label=_("Email"), widget=forms.EmailInput(attrs={'autofocus': True}))
-
-
-class PasswordResetForm(SetPasswordForm):
-    new_password1 = forms.CharField(
-        label=_("New password"),
-        widget=forms.PasswordInput(attrs={'autofocus': True}),
-        strip=False,
-    )
-    new_password2 = forms.CharField(
-        label=_("Confirm new password"),
-        widget=forms.PasswordInput,
-        strip=False,
-        help_text=_("Enter the same password as before, for verification."),
-    )
-
-    def clean_new_password2(self):
-        password1 = self.cleaned_data.get("new_password1")
-        password2 = self.cleaned_data.get("new_password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError(_("The two password fields didn't match."))
-        return password2
