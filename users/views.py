765931ebb07edc5ac0b235ecaf695da65c8e0857
Zachary Nelson <znelson@nebraskachildren.org>
2024-07-23 21:37:49 -0500

    manually login in the user for password reset

--- users/views.py
+++ users/views.py
@@ -1,8 +1,7 @@
-import logging
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
-from .forms import CustomAuthenticationForm
 from django.shortcuts import redirect
-from django.urls import reverse
+from .forms import CustomAuthenticationForm
 
 
 class CustomLoginView(LoginView):
@@ -11,14 +10,11 @@ class CustomLoginView(LoginView):
 
     def form_valid(self, form):
         user = form.get_user()
-        logging.info(f'User {user.email} must reset password: {user.must_reset_password}')
+        login(self.request, user)  # Manually log in the user
         if user.must_reset_password:
-            logging.info('Redirecting to password change')
-            self.request.session['reset_password_user_id'] = user.id
             return redirect('users:password_change')
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        logging.debug(f'Login view context: {context}')
         return context
