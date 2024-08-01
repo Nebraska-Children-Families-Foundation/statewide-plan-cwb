c12deaf7af88379689c05f8feebc0f946bdc1864
Zachary Nelson <znelson@nebraskachildren.org>
2024-08-01 16:57:47 -0500

    fix for community creator error

--- core/permissions.py
+++ core/permissions.py
@@ -1,12 +1,11 @@
 def has_edit_permission(user, action_step):
     # Check if the user is the creator
-    if action_step.creator == user:
+    if action_step.community_creator == user:
         return True
     # Check if the user is part of the same collaborative linked to the action step
     if user.community_collaborative == action_step.related_collaborative:
         return True
     return False
-
 
 def has_commitment_edit_permission(user, commitment):
     # Check if the user belongs to the System Partner associated with the commitment
