26f5a19b4848d214e2fefd09de5c113b84d27363
Zach Nelson <znelson@nebraskachildren.org>
2024-12-14 14:57:56 -0600

    use custom templates for password reset

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CustomLoginView, CustomPasswordChangeView
+from .views import CustomLoginView, CustomPasswordChangeView, CustomPasswordResetView
 from django.contrib.auth.views import (
     LogoutView, PasswordResetView, PasswordResetDoneView,
     PasswordResetConfirmView, PasswordResetCompleteView,
@@ -11,7 +11,7 @@ app_name = 'users'
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('password_reset/', PasswordResetView.as_view(), name='reset_password'),
+    path('password_reset/', CustomPasswordResetView.as_view(), name='reset_password'),
     path('password_reset_done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset_password_complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
